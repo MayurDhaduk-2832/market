d5f93b79e9d2a1b5b3b4fd10530829c9a8448225
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-07-15 21:47:48 +0530

    - dont move phone chages in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sellproducts/network/api_client.dart';
 import 'routes/app_pages.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Firebase.initializeApp(
 //    options: DefaultFirebaseOptions.currentPlatform,
       );
